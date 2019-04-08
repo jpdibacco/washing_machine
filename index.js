c27bea42a5fe9b32c1f0857d93258de5443cdb18
Juan Patricio Di Bacco <jpdibacco@gmail.com>
2019-04-08 21:14:23 +0200

    tested timer

--- index.js
+++ index.js
@@ -21,3 +21,15 @@ http.listen(3000, function(){
 });
 
 
+var countdown = 1000;
+setInterval(function() {
+  countdown--;
+  io.sockets.emit('timer', { countdown: countdown });
+}, 1000);
+
+io.sockets.on('connection', function (socket) {
+  socket.on('reset', function (data) {
+    countdown = 1000;
+    io.sockets.emit('timer', { countdown: countdown });
+  });
+})
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,9 @@
+var socket = io.connect('http://localhost:3000');
 
+socket.on('timer', function (data) {
+    $('#counter').html(data.countdown);
+});
+
+$('#reset').click(function() {
+    socket.emit('reset');
+});
