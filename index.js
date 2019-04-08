bdd8d701d47f6ab75b3835586668f3e3ea495acd
Juan Patricio Di Bacco <jpdibacco@gmail.com>
2019-04-08 21:04:21 +0200

    added countdown timer

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-//var app = require('express')();
-var http = require('http').Server(app);
 var express = require('express');
+var path = require('path');
 var app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static('public'));
+
+// var app = require('express')();
+ var http = require('http').Server(app);
+ var io = require('socket.io')(http);
+
+// app.get('/public', function(req, res){
+//   res.sendFile(__dirname + 'index.html');
+// });
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+
