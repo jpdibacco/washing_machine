d04d4253182585683c4dfe0278947a6b29fcae12
Juan Patricio Di Bacco <jpdibacco@gmail.com>
2019-04-09 00:06:04 +0200

    cleaning code

--- index.js
+++ index.js
@@ -2,32 +2,22 @@ var express = require('express');
 var path = require('path');
 var app = express();
 const PORT = process.env.PORT || 3000;
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-// var app = require('express')();
- var http = require('http').Server(app);
- var io = require('socket.io')(http);
-
-// app.get('/public', function(req, res){
-//   res.sendFile(__dirname + 'index.html');
-// });
 
 io.on('connection', function(socket){
   console.log('a user connected');
 });
-
 http.listen(PORT, function(){
   console.log('listening on *:3000');
 });
-
-
 var countdown = 1000;
 setInterval(function() {
   countdown--;
   io.sockets.emit('timer', { countdown: countdown });
 }, 1000);
-
 io.sockets.on('connection', function (socket) {
   socket.on('reset', function (data) {
     countdown = 1000;
