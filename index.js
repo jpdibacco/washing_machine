6ed7c0707a381261640788fb26506aacd1c3f181
Juan Patricio Di Bacco <jpdibacco@gmail.com>
2019-04-08 17:46:12 +0200

    moved all client folder to root

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var app = require('express')();
 var http = require('http').Server(app);
-
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/client/index.html');
 });
+app.use('/client', express.static(__dirname + '/css'));
+app.use('/client',express.static(__dirname + '/js'));
+// app.get('/', function(req, res){
+//   res.send('<h1>Hello world</h1>');
+// });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
